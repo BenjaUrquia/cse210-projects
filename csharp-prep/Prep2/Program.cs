7caa4b1a8d73d360ee2edf011f613d8ea0ffe4b6
BenjaUrquia <benjaminurquia2000@gmail.com>
2024-01-18 21:23:33 -0300

    W01 Prove: Developer—C# Programming

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string answer = Console.ReadLine();
+        double gradePercentage = Double.Parse(answer);
+        string letter = "";
+
+    if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+    else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+    else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+    else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+    else
+        {
+            letter = "F";
+        }
+
+    
+    Console.WriteLine($"Your letter grade is: {letter}");
+
+    
+    if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+    else
+        {
+            Console.WriteLine("You didn't pass the course.");
+        }
+
     }
 }
