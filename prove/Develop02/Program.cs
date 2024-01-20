d6f3647f00b257d01f21a4b4aa625caddb087584
BenjaUrquia <benjaminurquia2000@gmail.com>
2024-01-20 13:38:50 -0300

    W02 Prove: Developer—Journal

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -2,8 +2,20 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal journal = new Journal();
+
+        JournalEntry entry1 = new JournalEntry("2024-01-20", "Question 1", "Answer 1");
+        JournalEntry entry2 = new JournalEntry("2024-01-21", "Question 2", "Answer 2");
+
+        journal.AddEntry(entry1);
+        journal.AddEntry(entry2);
+
+        Console.WriteLine("Displaying all entries:\n");
+        journal.DisplayAll();
+
+        Console.WriteLine("Displaying entry at index 0:\n");
+        journal.DisplayEntry(0);
     }
 }
