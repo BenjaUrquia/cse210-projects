56c39e4e046bd74dfb0e87f31b9c1dc1bdae1a6d
BenjaUrquia <benjaminurquia2000@gmail.com>
2024-01-24 19:56:47 -0300

    W03 Prepare: Learning Activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,73 @@
 using System;
 
+class Fraction
+{
+    private int numerator;
+    private int denominator;
+
+    public Fraction()
+    {
+        numerator = 1;
+        denominator = 1;
+    }
+
+    public Fraction(int top)
+    {
+        numerator = top;
+        denominator = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        numerator = top;
+        denominator = bottom != 0 ? bottom : 1;
+    }
+
+    public int GetNumerator()
+    {
+        return numerator;
+    }
+
+    public void SetNumerator(int top)
+    {
+        numerator = top;
+    }
+
+    public int GetDenominator()
+    {
+        return denominator;
+    }
+
+    public void SetDenominator(int bottom)
+    {
+        denominator = bottom != 0 ? bottom : 1;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{numerator}/{denominator}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)numerator / denominator;
+    }
+}
+
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(6);
+        Fraction fraction3 = new Fraction(6, 7);
+
+        Console.WriteLine($"Initial Fraction 1: {fraction1.GetFractionString()}");
+        fraction1.SetNumerator(3);
+        fraction1.SetDenominator(4);
+        Console.WriteLine($"Updated Fraction 1: {fraction1.GetFractionString()}");
+
+        Console.WriteLine($"Decimal Value of Fraction 2: {fraction2.GetDecimalValue()}");
+        Console.WriteLine($"Decimal Value of Fraction 3: {fraction3.GetDecimalValue()}");
     }
 }
